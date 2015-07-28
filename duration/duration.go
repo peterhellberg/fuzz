a80e3cf4f412115c50efffe41ca3d33ab80b735e
Peter Hellberg <peter@c7.se>
2015-07-28 16:54:49 +0200

    Return 0 instead of -1 on error

--- duration/duration.go
+++ duration/duration.go
@@ -5,9 +5,9 @@ package duration
 import "github.com/peterhellberg/duration"
 
 func Fuzz(data []byte) int {
-	if _, err := duration.Parse(string(data)); err == nil {
-		return 1
+	if _, err := duration.Parse(string(data)); err != nil {
+		return 0
 	}
 
-	return -1
+	return 1
 }
